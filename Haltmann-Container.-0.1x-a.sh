7effa37c6495ac9cb4bcf19787b6b67554d3834d
Flames Elite <84363801+FlamesLLC@users.noreply.github.com>
2022-07-07 17:01:35 -0700

    Add files via upload

---
+++ Haltmann-Container.-0.1x-a.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+echo "################################
+# Copyright (c) 2014 - 2018 Flames  LLC
+"
+echo "Enter the IP address you wish to encrypt traffic to and from:"
+read ip_address
+
+iptables -A INPUT -s $ip_address -j DROP
+iptables -A OUTPUT -d $ip_address -j 
+## generate a encrypted rsa keypair so no one can see your IP address
+def ip_address
+    echo "${ip_address}" | openssl req \
+ -nodes=1 \
+echo "Syncing datasets..."
+ \crypto\rsa.pem \
+ "${ip_address}"
+   fi
+   echo "################################################################################################################################"
+   echo "v0.0.0.0" >>   /etc/ssh/sshd_config
+ ## ask a user if you want to revert the state of the router to your default state
+ if [[ "${ip_address}" == "127.0.0.1"
+     || "${ip_address}" == "192.168.0.1"
+     || "${ip_address}"
+     fi
+     echo "################################################################################################################################"    
+        echo "IPSC0 V0.1X HALTMANN TERMINAL DONE! "
